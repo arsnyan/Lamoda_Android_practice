ba4fce96d9ba35e56277bbb9e15ca23c1e8828f6
beautyadore <ars.nyan2002@gmail.com>
2023-09-18 17:12:31 +0300

    updated icons to fit the buttons and headers

--- app/src/main/java/com/arsnyan/lamodacopy/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/arsnyan/lamodacopy/ui/profile/ProfileFragment.kt
@@ -2,15 +2,19 @@ package com.arsnyan.lamodacopy.ui.profile
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.arsnyan.lamodacopy.BuildConfig
 import com.arsnyan.lamodacopy.R
 import com.arsnyan.lamodacopy.ui.customview.ProfileSectionCardView
 import com.arsnyan.lamodacopy.ui.lamodaclub.LamodaClubScreenFragment
@@ -20,6 +24,7 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.snackbar.Snackbar
 
 class ProfileFragment : Fragment() {
 
@@ -30,6 +35,8 @@ class ProfileFragment : Fragment() {
     private lateinit var viewModel: ProfileViewModel
     private lateinit var btnOrders: ProfileSectionCardView
     private lateinit var cardUserDiscount: MaterialCardView
+    private lateinit var txtAppVersion: TextView
+    private lateinit var btnUpdate: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,8 +50,11 @@ class ProfileFragment : Fragment() {
         viewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
         btnOrders = view.findViewById(R.id.btn_orders)
         cardUserDiscount = view.findViewById(R.id.cardview_user_discount)
+        txtAppVersion = view.findViewById(R.id.txt_app_version)
+        btnUpdate = view.findViewById(R.id.link_check_updates)
         val navController = Navigation.findNavController(requireView())
 
+        txtAppVersion.text = resources.getString(R.string.lbl_version, BuildConfig.VERSION_NAME)
         btnOrders.setValue(viewModel.ordersCount)
         cardUserDiscount.setOnClickListener {
             navController.navigate(R.id.action_navigation_profile_to_navigation_lamodaClubInfo)
